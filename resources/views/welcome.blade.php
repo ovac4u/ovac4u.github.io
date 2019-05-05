2d6a80000814fa63ee4d59987428dd648514dd96
Victor Ariama (ovac4u) www.ovac4u.com <iamovac@gmail.com>
2019-05-05 15:13:46 +0000

    Push code

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1538,5 +1538,19 @@
           ga('send', 'pageview');
         </script>
         <!-- End Google Analytics -->
+
+        <script type="text/javascript">
+            (function(window, document){
+              window._blogfoster=window._blogfoster || {};
+              window._blogfoster.insights=window._blogfoster.insights || {};
+              window._blogfoster.insights.websiteId=17411;
+              var t="https://insights.blogfoster.com/v1/" + window._blogfoster.insights.websiteId + ".js";
+              var e=document, s=e.createElement("script"), a=e.getElementsByTagName("script")[0];
+              s.type="text/javascript"; s.defer=true; s.async=true; s.src=t; a.parentNode.insertBefore(s,a);
+            })(window, document);
+          </script>
+        <noscript>
+            <p><img src="https://insights.blogfoster.com/v1/17411.png" style="border:0;" alt="" /></p>
+        </noscript>
     </body>
 </html>
